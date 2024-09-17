2aef39905c95ab9f7f5b288e9e52cbf153d16f0d
Ukana.Bastreet <s.ppp@bk.ru>
2024-09-17 19:37:56 +0300

    test logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private config: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const VERCEL_URL = process.env.VERCEL_URL;
+
+    console.log('VERCEL_URL: ', VERCEL_URL);
+    return VERCEL_URL;
+    // return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
 
   const BOT_TOKEN = process.env.BOT_TOKEN;
-  const PORT = process.env.PORT;
   const VERCEL_URL = process.env.VERCEL_URL;
 
-  await app.listen(process.env.PORT || 3000);
   console.log('VERCEL_URL: ', VERCEL_URL);
   console.log('PORT: ', PORT);
-
   console.log('process.env.VAR: ' + process.env.VAR || 'NO VAR');
   console.log('BOT_TOKEN: ' + BOT_TOKEN);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
